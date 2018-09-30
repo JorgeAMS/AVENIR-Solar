b83ebbf590e6b74027ec9bbc057396defa4e2023
Jorge Morales <jamorales516@icloud.com>
2018-09-30 13:40:40 -0500

    Anti-Cheats Activated

--- formulas.js
+++ formulas.js
@@ -23,6 +23,7 @@ var kilow,
  */
 
 function clickeado(){
+  
 
   porcentaje = document.getElementById("rangeVAL").value;
   console.log(porcentaje);
@@ -30,70 +31,75 @@ function clickeado(){
   kilow =  document.getElementById("kilowats").value;
   console.log(kilow);
   
-  if((((((kilow*porcentaje)*10)/30))/1500) < 1){
-    TipoSolucion=1;
+  if(kilow==null || kilow==0){
+    alert("Por favor ingrese los Kilowats consumidos, no ingrese puntos, rayas, comas o caracteres.");
   }
   else{
-    TipoSolucion=Math.round(((((((kilow*porcentaje)*10)/30))/1500)+1));
+
+    if((((((kilow*porcentaje)*10)/30))/1500) < 1){
+      TipoSolucion=1;
+    }
+    else{
+      TipoSolucion=Math.round(((((((kilow*porcentaje)*10)/30))/1500)+1));
+    }
+    Accesorios=TipoSolucion*6;
+
+    console.log("TipoSolucion: "+TipoSolucion);
+    console.log("Accesorios: "+Accesorios);
+
+    if(porcentaje<100){ //En Red
+      SolucionSolar=Math.round(((TipoSolucion*500000)/3050)+700);
+      //console.log("SolucionSolar: "+SolucionSolar);
+      ManoObra=Math.round(SolucionSolar*0.12);
+      //console.log("ManoObra: "+ManoObra);
+      Transporte=Math.round(SolucionSolar*0.07);
+      //console.log("Transporte: "+Transporte);
+      Almacenamiento="No aplica";
+      //console.log("Almacenamiento: "+Almacenamiento);
+      Mantenimiento=Math.round(SolucionSolar*0.03)+1;
+      //console.log("Mantenimiento: "+Mantenimiento);
+      Total=Math.round(SolucionSolar+ManoObra+Transporte+Mantenimiento);
+      //console.log("Total: "+Total);
+    }
+    if(porcentaje==100){ //Fuera de Red
+      SolucionSolar=Math.round((((((kilow*1000)/30)/1500)*500000)/3050)+200);
+      //console.log("SolucionSolar: "+SolucionSolar);
+      ManoObra=Math.round(SolucionSolar*0.12);
+      //console.log("ManoObra: "+ManoObra);
+      Almacenamiento=Math.round((((((kilow*1000)/30)/3.6)*2100)+((((kilow*1000)/30)/3.6)*2100)*0.15)/3050);
+      //console.log("Almacenamiento: "+Almacenamiento);
+      Transporte=Math.round(SolucionSolar*0.07);
+      //console.log("Transporte: "+Transporte);
+      Mantenimiento=Math.round((SolucionSolar+Almacenamiento)*0.03)+1;
+      //console.log("Mantenimiento: "+Mantenimiento);
+      Total=Math.round(SolucionSolar+ManoObra+Almacenamiento+Transporte+Mantenimiento);
+      //console.log("Total: "+Total);
+    }
+
+    $('#boxing').addClass("boxing");
+    $('#boxing').addClass("animatedIN");
+    $('#boxing').addClass("fadeIn");
+    document.getElementById('TipoSolucion1').innerHTML="Tipo Solución: ";
+    document.getElementById('Accesorios1').innerHTML="Accesorios: ";
+    document.getElementById('SolucionSolar1').innerHTML="Solución Solar: ";
+    document.getElementById('ManoObra1').innerHTML="Mano de Obra: ";
+    document.getElementById('Almacenamiento1').innerHTML="Modulo Almacenamiento: ";
+    document.getElementById('Transporte1').innerHTML="Transporte: ";
+    document.getElementById('Asesoria1').innerHTML="Asesoria - Soporte: ";
+    document.getElementById('Mantenimiento1').innerHTML="Mantenimiento: ";
+    document.getElementById('Total1').innerHTML="Total: ";
+
+    document.getElementById('TipoSolucion2').innerHTML=TipoSolucion;
+    document.getElementById('Accesorios2').innerHTML=Accesorios;
+    document.getElementById('SolucionSolar2').innerHTML=SolucionSolar;
+    document.getElementById('ManoObra2').innerHTML=ManoObra;
+    document.getElementById('Almacenamiento2').innerHTML=Almacenamiento;
+    document.getElementById('Transporte2').innerHTML=Transporte;
+    document.getElementById('Asesoria2').innerHTML="Gratis!";
+    document.getElementById('Mantenimiento2').innerHTML=Mantenimiento;
+    document.getElementById('Total2').innerHTML=Total;
+
   }
-  Accesorios=TipoSolucion*6;
-
-  console.log("TipoSolucion: "+TipoSolucion);
-  console.log("Accesorios: "+Accesorios);
-
-  if(porcentaje<100){ //En Red
-    SolucionSolar=Math.round(((TipoSolucion*500000)/3050)+700);
-    //console.log("SolucionSolar: "+SolucionSolar);
-    ManoObra=Math.round(SolucionSolar*0.12);
-    //console.log("ManoObra: "+ManoObra);
-    Transporte=Math.round(SolucionSolar*0.07);
-    //console.log("Transporte: "+Transporte);
-    Almacenamiento="No aplica";
-    //console.log("Almacenamiento: "+Almacenamiento);
-    Mantenimiento=Math.round(SolucionSolar*0.03)+1;
-    //console.log("Mantenimiento: "+Mantenimiento);
-    Total=Math.round(SolucionSolar+ManoObra+Transporte+Mantenimiento);
-    //console.log("Total: "+Total);
-  }
-  if(porcentaje==100){ //Fuera de Red
-    SolucionSolar=Math.round((((((kilow*1000)/30)/1500)*500000)/3050)+200);
-    //console.log("SolucionSolar: "+SolucionSolar);
-    ManoObra=Math.round(SolucionSolar*0.12);
-    //console.log("ManoObra: "+ManoObra);
-    Almacenamiento=Math.round((((((kilow*1000)/30)/3.6)*2100)+((((kilow*1000)/30)/3.6)*2100)*0.15)/3050);
-    //console.log("Almacenamiento: "+Almacenamiento);
-    Transporte=Math.round(SolucionSolar*0.07);
-    //console.log("Transporte: "+Transporte);
-    Mantenimiento=Math.round((SolucionSolar+Almacenamiento)*0.03)+1;
-    //console.log("Mantenimiento: "+Mantenimiento);
-    Total=Math.round(SolucionSolar+ManoObra+Almacenamiento+Transporte+Mantenimiento);
-    //console.log("Total: "+Total);
-  }
-
-  $('#boxing').addClass("boxing");
-  $('#boxing').addClass("animatedIN");
-  $('#boxing').addClass("fadeIn");
-  document.getElementById('TipoSolucion1').innerHTML="Tipo Solución: ";
-  document.getElementById('Accesorios1').innerHTML="Accesorios: ";
-  document.getElementById('SolucionSolar1').innerHTML="Solución Solar: ";
-  document.getElementById('ManoObra1').innerHTML="Mano de Obra: ";
-  document.getElementById('Almacenamiento1').innerHTML="Modulo Almacenamiento: ";
-  document.getElementById('Transporte1').innerHTML="Transporte: ";
-  document.getElementById('Asesoria1').innerHTML="Asesoria - Soporte: ";
-  document.getElementById('Mantenimiento1').innerHTML="Mantenimiento: ";
-  document.getElementById('Total1').innerHTML="Total: ";
-
-  document.getElementById('TipoSolucion2').innerHTML=TipoSolucion;
-  document.getElementById('Accesorios2').innerHTML=Accesorios;
-  document.getElementById('SolucionSolar2').innerHTML=SolucionSolar;
-  document.getElementById('ManoObra2').innerHTML=ManoObra;
-  document.getElementById('Almacenamiento2').innerHTML=Almacenamiento;
-  document.getElementById('Transporte2').innerHTML=Transporte;
-  document.getElementById('Asesoria2').innerHTML="Gratis!";
-  document.getElementById('Mantenimiento2').innerHTML=Mantenimiento;
-  document.getElementById('Total2').innerHTML=Total;
-
-
 
 }
 
